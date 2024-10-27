13071789a89bf325aa705fc5176cc0700066baf1
brian ruddy <brian@ruddy.io>
2024-10-26 23:12:24 -0400

    kill serviceworker cache

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 
 import App from '@/App.vue';
-import '@/registerServiceWorker';
+// import '@/registerServiceWorker';
 import router from '@/router';
 import store from '@/store';
 
